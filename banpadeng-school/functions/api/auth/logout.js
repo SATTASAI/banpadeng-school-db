85bb56961ae5c68bcbab0d81f65ef5da0957419b
SATTASAI <sattawat.imc@gmail.com>
2026-09-12 12:08:56 +0700

    Delete banpadeng-school/functions directory

--- banpadeng-school/functions/api/auth/logout.js
+++
@@ -1,6 +0,0 @@
-import { buildClearCookie } from "../../_lib/crypto.js";
-import { jsonResponse } from "../../_lib/auth.js";
-
-export async function onRequestPost() {
-  return jsonResponse({ ok: true }, 200, { "Set-Cookie": buildClearCookie() });
-}
